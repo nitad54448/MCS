29bd98fbc8c596cc43898e88cea7807c2c789325
nitad54448 <ndragoe@gmail.com>
2026-04-10 08:59:32 +0200

    4.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     "./src/**/*.{html,js}"  // Scans all HTML/JS files inside the src folder
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+      colors: {
+        sci: {
+          50: '#f0f9ff',
+          100: '#e0f2fe',
+          200: '#bae6fd',
+          500: '#0ea5e9',
+          600: '#0284c7',
+          800: '#075985',
+          900: '#0c4a6e',
+        }
+      }
+    },
   },
   plugins: [],
 }
